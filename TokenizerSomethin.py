b5e763b709f2b3d24893158ed63d6d6aa25122c4
Landonie314 <trumpet.landon@gmail.com>
2023-10-04 23:54:54 -0500

    Big Progress
    I am tired, implemented split and identification of Token Class logic

--- TokenizerSomethin.py
+++ TokenizerSomethin.py
@@ -4,10 +4,14 @@ file1 = open("Main.jack", "r")
 file2 = open("SquareGame.jack", "r")
 
 #Token Classes
-Comments = ["//", "/*", "/**", "*/", "**/"]
-Symbols = ["(", ")", "[", "]", "{", "}", ",", ";", "=", ".", "+", "-", "*", "/", "&", "|", "~", "<", ">"]
-Markup = ["&lt;", "&gt;", "&quot;", "&amp;"]
-Reserve = ["class", "constructor", "method", "funtion", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "false", "null", "this"]
+comments = ["//", "/*", "/**", "*/", "**/"]
+symbols = ["(", ")", "[", "]", "{", "}", ",", ";", "=", ".", "+", "-", "*", "/", "&", "|", "~", "<", ">"]
+specialSymbol = ["<", ">", "\"", "&"]
+markup = ["&lt;", "&gt;", "&quot;", "&amp;"]
+reserve = ["class", "constructor", "method", "funtion", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "false", "null", "this", "main"]
+
+#list to store different tokens for each line
+smolTemp = []
 
 
 #stores the file into a list
@@ -16,15 +20,33 @@ tempTok = file1.readlines()
 
 #for each element in the list (Line)
 for x in tempTok:
+    #splitting each line into tokens
+    smolTemp = x.split()
+    #each token
+    for i in smolTemp:
+        #What class does the token belong to?
+        if i in comments:
+            print("<comment> " + i + " </comment>")
+        if i in symbols:
+            print("<symbol> " + i + " </symbol>")
+        if i in specialSymbol:
+            if i == "<":
+                print("<symbol> " + "&lt;" + " </symbol>")
+            if i == ">":
+                print("<symbol> " + "&gt;" + " </symbol>")
+            if i == "\"":
+                print("<symbol> " + "&quot;" + " </symbol>")
+            if i == "&":
+                print("<symbol> " + "&amp;" + " </symbol>")
+        if i in reserve:
+            print("<keyword>" + i + "</keyword>")
     
-    if (x[0] == '/'):
-       continue
     
-    print(x)
     #for i in x:
     #   print(i)
 
-
+for i in smolTemp:
+    print(i)
    
     
 
