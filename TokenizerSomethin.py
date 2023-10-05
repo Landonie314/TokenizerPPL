f75dba589124aa73f94b06b96aaf8ee6b5c400b9
Landonie314 <trumpet.landon@gmail.com>
2023-10-05 11:38:41 -0500

    Comments Work
    Now all that is left to do is implement for string constants

--- TokenizerSomethin.py
+++ TokenizerSomethin.py
@@ -6,7 +6,7 @@ file1 = open("Main.jack", "r")
 file2 = open("SquareGame.jack", "r")
 
 #Token Classes
-comments = ["//", "/*", "/**", "*/", "**/"]
+comments = ["//", "/*", "/**", "*/"]
 symbols = ["(", ")", "[", "]", "{", "}", ",", ";", "=", ".", "+", "-", "*", "/", "|", "~"]
 specialSymbol = ["<", ">", "\"", "&"]
 markup = ["&lt;", "&gt;", "&quot;", "&amp;"]
@@ -14,27 +14,30 @@ reserve = ["class", "constructor", "method", "function", "int", "boolean", "char
 
 #list to store different tokens for each line
 smolTemp = []
-tester = []
-
 #stores the file into a list
 tempTok =[] 
-tempTok = file1.readlines()
+tempTok = file2.readlines()
 
 #for each element in the list (Line)
 for x in tempTok:
     print(x)
     #splitting each line into tokens
-    #smolTemp = x.split()
     smolTemp = filter(None, re.split(r'\s|([^\w@#/*"?])', x))
     #for i in smolTemp:
-    #    print(i)
-    
+    #print(i)
     #each token
     for i in smolTemp:
         #What class does the token belong to?
         if i in comments:
             print("<comment> " + i + " </comment>")
-        elif i in symbols:
+            #Only reads the comment line tokens
+            if(i == "//"):
+                break
+            else:
+                print("<comment> " + "*/" + " </comment>")
+                break
+                
+        if i in symbols:
             print("<symbol> " + i + " </symbol>")
         elif i in specialSymbol:
             if i == "<":
@@ -46,26 +49,13 @@ for x in tempTok:
             elif i == "&":
                 print("<symbol> " + "&amp;" + " </symbol>")
         elif i in reserve:
-            print("<keyword>" + i + "</keyword>")
+            print("<keyword> " + i + " </keyword>")
         else:
-            print("<idenifier>" + i + "</identifier>")
+            print("<identifier> " + i + " </identifier>")
+        
     
     
-    #for i in x:
-    #   print(i)
-
-
-for x in smolTemp:
-    tester = x.split(";")
-    for i in tester:
-        print(i)
-
     
-
-
-
-
-
 #Closing files
-file1.close
-file2.close
+file1.close()
+file2.close()
