740bbe95a4b3ad261c1e7777062baa2b855ce7c7
Landonie314 <trumpet.landon@gmail.com>
2023-10-04 23:29:18 -0500

    More Token Classes
    Data Loss Scary

--- TokenizerSomethin.py
+++ TokenizerSomethin.py
@@ -6,21 +6,23 @@ file2 = open("SquareGame.jack", "r")
 #Token Classes
 Comments = ["//", "/*", "/**", "*/", "**/"]
 Symbols = ["(", ")", "[", "]", "{", "}", ",", ";", "=", ".", "+", "-", "*", "/", "&", "|", "~", "<", ">"]
+Markup = ["&lt;", "&gt;", "&quot;", "&amp;"]
+Reserve = ["class", "constructor", "method", "funtion", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "false", "null", "this"]
 
 
+#stores the file into a list
 tempTok =[] 
 tempTok = file1.readlines()
 
+#for each element in the list (Line)
 for x in tempTok:
-    if (x == ""):
-        continue
     
-    #if (x[0] == '/'):
-    #    continue
+    if (x[0] == '/'):
+       continue
     
     print(x)
     #for i in x:
-   #     print(i)
+    #   print(i)
 
 
    
