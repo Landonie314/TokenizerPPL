ee9cd23c38d61ed2e9ff3acfe85126663eb859f2
Landonie314 <trumpet.landon@gmail.com>
2023-10-05 17:52:11 -0500

    Fully Functional for File1
    Works fully for file 1. Still needs testing for file 2. Then need to make it output to .xml file

--- TokenizerSomethin.py
+++ TokenizerSomethin.py
@@ -14,47 +14,59 @@ reserve = ["class", "constructor", "method", "function", "int", "boolean", "char
 
 #list to store different tokens for each line
 smolTemp = []
+tempNumer = 0
 #stores the file into a list
 tempTok =[] 
-tempTok = file2.readlines()
+tempTok = file1.readlines()
 
 #for each element in the list (Line)
 for x in tempTok:
     print(x)
     #splitting each line into tokens
-    smolTemp = filter(None, re.split(r'\s|([^\w@#/*"?])', x))
+    smolTemp = filter(None, re.split(r'\s|([^\w@#/*?])', x))
     #for i in smolTemp:
-    #print(i)
+    tempWord = ""
     #each token
     for i in smolTemp:
         #What class does the token belong to?
-        if i in comments:
-            print("<comment> " + i + " </comment>")
-            #Only reads the comment line tokens
-            if(i == "//"):
-                break
-            else:
-                print("<comment> " + "*/" + " </comment>")
-                break
+        if(tempNumer % 2 == 0):
+            if i in comments:
+                print("<comment> " + i + " </comment>")
+                #Only reads the comment line tokens
+                if(i == "//"):
+                    break
+                else:
+                    print("<comment> " + "*/" + " </comment>")
+                    break
                 
-        if i in symbols:
-            print("<symbol> " + i + " </symbol>")
-        elif i in specialSymbol:
-            if i == "<":
-                print("<symbol> " + "&lt;" + " </symbol>")
-            elif i == ">":
-                print("<symbol> " + "&gt;" + " </symbol>")
-            elif i == "\"":
-                print("<symbol> " + "&quot;" + " </symbol>")
-            elif i == "&":
-                print("<symbol> " + "&amp;" + " </symbol>")
-        elif i in reserve:
-            print("<keyword> " + i + " </keyword>")
+            if i in symbols:
+                print("<symbol> " + i + " </symbol>")
+            elif i in specialSymbol:
+                if i == "<":
+                    print("<symbol> " + "&lt;" + " </symbol>")
+                elif i == ">":
+                    print("<symbol> " + "&gt;" + " </symbol>")
+                elif i == "\"":
+                    print("<symbol> " + "&quot;" + " </symbol>")
+                    tempNumer += 1
+                    continue
+                elif i == "&":
+                    print("<symbol> " + "&amp;" + " </symbol>")
+            elif i in reserve:
+                print("<keyword> " + i + " </keyword>")
+            elif i.isdigit():
+                if(int(i) >= 0):
+                    print("<integerConstant " + i + " </integerConstant>") 
+            else:
+                print("<identifier> " + i + " </identifier>")
         else:
-            print("<identifier> " + i + " </identifier>")
-        
-    
-    
+            if(i == "\""):
+                tempNumer += 1
+                print("<stringConstant " + tempWord + " </stringConstant>")
+                print("<symbol> " + "&quot;" + " </symbol>")
+            tempWord = tempWord + " " + i
+            continue
+          
     
 #Closing files
 file1.close()
