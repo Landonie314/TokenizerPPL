9d8e3ddd18fba57c155eaf7c25f64cc932e86653
Landonie314 <trumpet.landon@gmail.com>
2023-10-05 20:31:39 -0500

    Final for me for now
    Did not get multi line comments to be skipped

--- TokenizerSomethin.py
+++ TokenizerSomethin.py
@@ -4,6 +4,9 @@ import re
 re.compile('<title>(.*)</title>')
 file1 = open("Main.jack", "r")
 file2 = open("SquareGame.jack", "r")
+f1 = open("MainT.xml", "w")
+f2 = open("SquareGameT.xml", "w")
+
 
 #Token Classes
 comments = ["//", "/*", "/**", "*/"]
@@ -18,11 +21,11 @@ smolTemp = []
 tempNumer = 0
 #stores the file into a list
 tempTok =[] 
-tempTok = file2.readlines()
+tempTok = file1.readlines()
 
 #for each element in the list (Line)
 for x in tempTok:
-    print(x)
+    #print(x)
     #splitting each line into tokens
     smolTemp = filter(None, re.split(r'\s|([^\w@#/*?])', x))
     #for i in smolTemp:
@@ -33,41 +36,41 @@ for x in tempTok:
         #What class does the token belong to?
         if(tempNumer % 2 == 0):
             if i in comments:
-                print("<comment> " + i + " </comment>")
+                f1.write("<comment> " + i + " </comment>")
                 #Only reads the comment line tokens
                 if(i == "//"):
                     break
                 else:
-                    print("<comment> " + "*/" + " </comment>")
+                    f1.write("<comment> " + "*/" + " </comment>")
                     break           
             if i in symbols:
-                print("<symbol> " + i + " </symbol>")
+                f1.write("<symbol> " + i + " </symbol>")
             elif i in specialSymbol:
                 if i == "<":
-                    print("<symbol> " + "&lt;" + " </symbol>")
+                    f1.write("<symbol> " + "&lt;" + " </symbol>")
                 elif i == ">":
-                    print("<symbol> " + "&gt;" + " </symbol>")
+                    f1.write("<symbol> " + "&gt;" + " </symbol>")
                 elif i == "\"":
-                    print("<symbol> " + "&quot;" + " </symbol>")
+                    f1.write("<symbol> " + "&quot;" + " </symbol>")
                     tempNumer += 1
                     continue
                 elif i == "&":
-                    print("<symbol> " + "&amp;" + " </symbol>")
+                    f1.write("<symbol> " + "&amp;" + " </symbol>")
             elif i in reserve:
-                print("<keyword> " + i + " </keyword>")
+                f1.write("<keyword> " + i + " </keyword>")
             #numberConstants
             elif i.isdigit():
                 if(int(i) >= 0):
-                    print("<integerConstant " + i + " </integerConstant>") 
+                    f1.write("<integerConstant " + i + " </integerConstant>") 
             else:
-                print("<identifier> " + i + " </identifier>")
+                f1.write("<identifier> " + i + " </identifier>")
         #if currently inside of double quotes
         else:
             if(i == "\""):
                 tempNumer += 1
                 #build the String constants
-                print("<stringConstant " + tempWord + " </stringConstant>")
-                print("<symbol> " + "&quot;" + " </symbol>")
+                f1.write("<stringConstant " + tempWord + " </stringConstant>")
+                f1.write("<symbol> " + "&quot;" + " </symbol>")
             tempWord = tempWord + " " + i
             #next iteration
             continue
@@ -75,3 +78,5 @@ for x in tempTok:
 #Closing files
 file1.close()
 file2.close()
+f1.close()
+f2.close()
